7145e9086847e9be76ff07af6d362e991d5c5fdb
hnayoung <hnayoung0329@gmail.com>
2024-08-14 01:18:31 +0900

    zero commit

--- src/component/Box.js
+++ src/component/Box.js
@@ -1,26 +1,12 @@
 import React from 'react'
 
 const Box = (props) => {
-    let result;
-    if ( 
-      // 1. 카드가 computer 카드인가?
-      // 2. 결과가 비긴 건 아닌가?
-      // 3. props.result에 값이 있는가? 
-      props.title === "Computer" && 
-      props.result !== "tie" && 
-      props.result !== "" ) {
-      result = props.result === "win" ? "lose" : "win";
-    }else {
-      // 위의 경우가 아님 -> props에 전달된 결과를 그대로 씀 
-      result = props.result;
-    }
-
+      console.log("props", props);
   return (
-    <div className={`box ${result}`}>
+    <div className="box">
         <h1>{props.title}</h1>
-        <h2>{props.item && props.item.name}</h2>
         <img className="item-img" src={props.item && props.item.img} />
-        <h2>{result}</h2>
+        <h2>Win</h2>
     </div>
   );
 };
